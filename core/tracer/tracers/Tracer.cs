eca619f1fd7562df52cbd819086e1461541abb0b
DashaTsybulkina <cybulkinadara@gmail.com>
2022-09-30 17:32:49 +0300

    correct tracer

--- core/tracer/tracers/Tracer.cs
+++ core/tracer/tracers/Tracer.cs
@@ -10,6 +10,7 @@ namespace core.tracers
     public class Tracer: ITracer
     {
         private ConcurrentDictionary<int, ConcurrentStack<MethodTracerResult>> threads;
+        private object lockObject = new object();
 
         public Tracer() { 
             threads = new ConcurrentDictionary<int, ConcurrentStack<MethodTracerResult>>();
@@ -17,43 +18,52 @@ namespace core.tracers
 
         public void StartTrace()
         {
-            int threedId = Thread.CurrentThread.ManagedThreadId;
-            StackFrame[] stackFrames = new StackTrace(true).GetFrames();
-            var stackFrame = stackFrames[1];
-            ConcurrentStack<MethodTracerResult> stack = threads.GetOrAdd(threedId, new ConcurrentStack<MethodTracerResult>());
-            MethodTracerResult method = new MethodTracerResult(stackFrame.GetMethod().DeclaringType.FullName, stackFrame.GetMethod().Name);
-            stack.Push(method);
-            method.StartTimer();
+            lock (lockObject)
+            {
+                int threedId = Thread.CurrentThread.ManagedThreadId;
+                StackFrame[] stackFrames = new StackTrace(true).GetFrames();
+                var stackFrame = stackFrames[1];
+                ConcurrentStack<MethodTracerResult> stack = threads.GetOrAdd(threedId, new ConcurrentStack<MethodTracerResult>());
+                MethodTracerResult method = new MethodTracerResult(stackFrame.GetMethod().DeclaringType.FullName, stackFrame.GetMethod().Name);
+                stack.Push(method);
+                method.StartTimer();
+            }
         }
 
         public void StopTrace()
         {
-            var id = Thread.CurrentThread.ManagedThreadId;
-            ConcurrentStack<MethodTracerResult> stack = threads.GetOrAdd(id, new ConcurrentStack<MethodTracerResult>());
-            stack.TryPop(out var method);
-            method.StopTimer();
-            if (stack.TryPeek(out var parent))
+            lock (lockObject)
             {
-                parent.AddMethod(method);
-            }
-            else
-            {
-                stack.Push(method);
+                var id = Thread.CurrentThread.ManagedThreadId;
+                ConcurrentStack<MethodTracerResult> stack = threads.GetOrAdd(id, new ConcurrentStack<MethodTracerResult>());
+                stack.TryPop(out var method);
+                method.StopTimer();
+                if (stack.TryPeek(out var parent))
+                {
+                    parent.AddMethod(method);
+                }
+                else
+                {
+                    stack.Push(method);
+                }
             }
         }
 
         public TracerResult GetTraceResult() 
         {
-            List<TracerThreadResult> results = new List<TracerThreadResult>();
-            foreach (var id in threads.Keys)
+            lock (lockObject)
             {
-                TracerThreadResult thread = new TracerThreadResult(id);
-                ConcurrentStack<MethodTracerResult> methods = threads.GetOrAdd(id, new ConcurrentStack<MethodTracerResult>());
-                thread.AddMethods(methods.ToList());
-                results.Add(thread);
+                List<TracerThreadResult> results = new List<TracerThreadResult>();
+                foreach (var id in threads.Keys)
+                {
+                    TracerThreadResult thread = new TracerThreadResult(id);
+                    ConcurrentStack<MethodTracerResult> methods = threads.GetOrAdd(id, new ConcurrentStack<MethodTracerResult>());
+                    thread.AddMethods(methods.ToList());
+                    results.Add(thread);
+                }
+                TracerResult resultTrace = new TracerResult(results);
+                return resultTrace;
             }
-            TracerResult resultTrace = new TracerResult(results);
-            return resultTrace;
         }
     }
 }
